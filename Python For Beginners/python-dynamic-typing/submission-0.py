6d3ee70f03b431e62362b05ae00c08437ab77855
Ramamoorthy S <69966109+ramamoorthy07@users.noreply.github.com>
2026-09-02 23:16:40 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 7
+print(type(variable))
+variable = 7.0
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Ram"
+print(type(variable))
+variable = [1, 2, 3]
+print(type(variable))
